a2a9339f840c72c492be0afadb07bd801c47c0f6
cnzau <clement.nzau@gmail.com>
2016-06-09 03:22:44 +0300

    initial commit

---
+++ app/src/main/java/ke/co/mobwebz/smartflash/MainActivity.java
@@ -0,0 +1,13 @@
+package ke.co.mobwebz.smartflash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
