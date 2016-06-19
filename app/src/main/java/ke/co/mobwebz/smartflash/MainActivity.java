bf47aa4459fdacedfc95486e0f202f1e35e435ee
cnzau <clement.nzau@gmail.com>
2016-06-19 16:09:41 +0300

    turn flashlight off

--- app/src/main/java/ke/co/mobwebz/smartflash/MainActivity.java
+++ app/src/main/java/ke/co/mobwebz/smartflash/MainActivity.java
@@ -88,4 +88,25 @@ public class MainActivity extends Activity {
         }
 
     }
+    /*
+    * Turning Off flash
+    */
+    private void turnOffFlash() {
+        if (isFlashOn) {
+            if (camera == null || params == null) {
+                return;
+            }
+            // play sound
+            playSound();
+
+            params = camera.getParameters();
+            params.setFlashMode(Parameters.FLASH_MODE_OFF);
+            camera.setParameters(params);
+            camera.stopPreview();
+            isFlashOn = false;
+
+            // changing button/switch image
+            toggleButtonImage();
+        }
+    }
 }
